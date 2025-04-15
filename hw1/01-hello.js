b95469595488b4aa3b9182a8c0c02bace197840f
Gamaliel Rojas <jarib042689@gmail.com>
2025-04-15 16:02:59 -0700

    Final Draft

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,10 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
 }
+
+console.log("Testing formatter and DevTools log visibility.");
+console.log(
+  " My name is Gamaliel Rojas and I will be attending CS 463/563: Intro to Web Development this quarter. I am getting started with the first homework assignment. The first thing I will l do is figure out how to run code checks for accessibility, formatting, and valid HTML and CSS code."
+);
